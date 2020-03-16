5b0a0fd77e60c18a28337bfe46637a1c8c92a600
Craig Booker <craig@arcadiasoftworks.com>
2020-03-16 17:02:30 -0500

    Added some basic state to the navBar. Now login, register show to a guest, otherwise it shows Home, About, and then the username with the option to logout.

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 
 import {
 	REGISTER_SUCCESS,
@@ -26,7 +27,22 @@ const AuthState = props => {
 	const [state, dispatch] = useReducer(authReducer, initialState);
 
 	// Load User
-	const loadUser = () => console.log('LoadUser');
+	const loadUser = async () => {
+		if (localStorage.token) {
+			setAuthToken(localStorage.token);
+		}
+
+		try {
+			const res = await axios.get('/api/auth');
+
+			dispatch({
+				type: USER_LOADED,
+				payload: res.data
+			});
+		} catch (err) {
+			dispatch({ type: AUTH_ERROR });
+		}
+	};
 
 	// Register User
 	const register = async formData => {
@@ -43,6 +59,8 @@ const AuthState = props => {
 				type: REGISTER_SUCCESS,
 				payload: res.data
 			});
+
+			loadUser();
 		} catch (err) {
 			dispatch({
 				type: REGISTER_FAIL,
@@ -52,10 +70,35 @@ const AuthState = props => {
 	};
 
 	// Login User
-	const login = () => console.log('Login');
+	const login = async formData => {
+		const config = {
+			headers: {
+				'Context-Type': 'application/json'
+			}
+		};
+
+		try {
+			const res = await axios.post('/api/auth', formData, config);
+
+			dispatch({
+				type: LOGIN_SUCCESS,
+				payload: res.data
+			});
+
+			loadUser();
+		} catch (err) {
+			dispatch({
+				type: LOGIN_FAIL,
+				payload: err.response.data.msg
+			});
+		}
+	};
 
 	// Logout
-	const logout = () => console.log('Logout');
+	const logout = () =>
+		dispatch({
+			type: LOGOUT
+		});
 
 	// Clear Errors
 	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
